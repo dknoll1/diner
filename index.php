682cf0e8570693b9bfcf013ca7fb4dea6a9591d4
Daniel Knoll <knollmdan@gmail.com>
2023-01-17 16:51:15 -0500

    controller homepage css file created

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// This is my controller
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once('vendor/autoload.php');
+
+// Instantiate F3 Base Class
+$f3 = Base::instance();
+
+// Define a default route (328/diner)
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/diner-home.html');
+    echo 'My Diner';
+});
+
+// Run Fat Free
+$f3->run();
+
+?>
