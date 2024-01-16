6e36a957cab690d07b06b0bda02016abe9b2be59
Daniel Knoll <knollmdan@gmail.com>
2024-01-16 12:56:07 -0500

    updated index to be a proper f3 controller

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-    echo "Hello";
+/* Daniel Knoll
+ * January 2024
+ * https://dknoll.greenriverdev.com/328/diner/
+ * this is my controller for the diner app
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once ('vendor/autoload.php');
+
+// instantiate fat-free framework (f3)
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo "My Diner";
+});
+
+// run f3
+$f3->run();
